f68f6313477a458845ac429418883cf2365bf414
jyoti <jpmahali18@gmail.com>
2020-05-29 16:23:51 +0530

    added dependencies

--- Alten/src/test/java/sample/CreationCompte.java
+++ Alten/src/test/java/sample/CreationCompte.java
@@ -1,9 +1,9 @@
 package sample;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import java.io.*;
+import org.junit.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.*;
+import org.openqa.selenium.remote.*;
 import org.testng.annotations.Test;
 
 
